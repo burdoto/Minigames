f6ba99cd9ea52230d4f5aa732ed41adc9de9c681
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-03-07 17:16:53 +0100

    Update Program.cs

--- FalloutHacking/Program.cs
+++ FalloutHacking/Program.cs
@@ -1,9 +1,20 @@
-﻿using comroid.csapi.common;
+﻿using System.Text.RegularExpressions;
+using comroid.csapi.common;
 
 namespace FalloutHacking;
 
+public enum Difficulty : int
+{
+    Peaceful = int.MaxValue,
+    Easy = 4,
+    Normal = 3,
+    Hard = 2
+}
+
 public class Program
 {
+    private readonly Difficulty _difficulty;
+
     public static readonly Dictionary<int, string[]> WordRepo = new()
     {
         {
@@ -17,24 +28,67 @@ public class Program
         }
     };
 
-    public static void Main(string[] args) => new Program(4).play();
+    public static void Main(string[] args) => new Program(Difficulty.Easy, 4).play();
 
-    public string[] Words { get; set; }
+    public string[] Words { get; }
+    public int UseIndex { get; }
 
-    private Program(int letters, int wordCount = 8)
+    private Program(Difficulty difficulty, int letters, int wordCount = 8)
     {
-        this.Words = WordRepo[letters].Shuffle().ToArray()[..wordCount];
+        this._difficulty = difficulty;
         this.UseIndex = new Random().Next(0, wordCount);
+        do this.Words = WordRepo[letters].Shuffle().ToArray()[..wordCount];
+        while (Words.Count(w => CompareWords(Words[UseIndex], w) == 0) > wordCount * ((int)difficulty / wordCount));
     }
 
     private void play()
     {
-        var attempts = 3;
+        var attemptsLeft = (int)_difficulty;
         var hacked = false;
 
-        while (!hacked && attempts > 0)
+        void PrintWords()
         {
-            var input = Console.ReadLine();
+            Console.WriteLine("Words available:");
+            for (var i = 0; i < Words.Length; i++)
+            {
+                var word = Words[i];
+                Console.WriteLine($"\t- {i}:\t{word}");
+            }
         }
+        
+        PrintWords();
+
+        while (!hacked && attemptsLeft > 0)
+        {
+
+            Console.Write("> ");
+            var input = Console.ReadLine()!;
+            var solution = Words[UseIndex];
+            string used;
+            if (Regex.IsMatch(input, "\\d+"))
+                used = Words[int.Parse(input)];
+            else if (solution.Equals(input, StringComparison.InvariantCultureIgnoreCase))
+                used = solution;
+            else used = input;
+            
+            hacked = used == solution;
+
+            if (!hacked)
+            {
+                Console.WriteLine($"Invalid [0x{CompareWords(solution, used):X}]; remaining {--attemptsLeft}");
+            } else Console.WriteLine("You're in!");
+        }
+        
+        if (!hacked)
+            Console.WriteLine("Locked out");
+    }
+
+    private static int CompareWords(string solution, string used)
+    {
+        var match = 0;
+        for (var i = 0; i < Math.Min(solution.Length, used.Length); i++)
+            if (solution[i] == used[i])
+                match++;
+        return match;
     }
 }
