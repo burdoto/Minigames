e5d66b9558aac8313ad2e3a36a3491c1f7cabf27
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-03-09 21:01:20 +0100

    asd

---
+++ ESim/ESim.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Principal;
+using SFML.Window;
+
+namespace comroid.ESim;
+
+public class ESim
+{
+    public static void Main(string[] args)
+    {
+        var win = new Window(new VideoMode(600, 400), "ESim");
+        win.SetVerticalSyncEnabled(true);
+        win.SetFramerateLimit(60);
+
+        while (win.IsOpen)
+        {
+            win.
+
+            win.DispatchEvents();
+        }
+    }
+}
