7a2d83b0b395aa246ae9a3c7a86d978762d332bd
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-03-14 20:15:58 +0100

    Update PlayBall.cs

--- Brick.NET/PlayBall.cs
+++ Brick.NET/PlayBall.cs
@@ -6,21 +6,26 @@ namespace Brick.NET;
 
 public class PlayBall : GameObject
 {
+    public const int Radius = 15;
     private readonly Rigidbody rigidbody;
 
     public PlayBall(GameBase game) : base(game)
     {
-        Add(new Circle(this) { Radius = 15 });
-        this.rigidbody = Add<Rigidbody>()!;
+        var c = Add<Circle>()!;
+        c.Radius = Radius;
+        c.Add<Circle.Collider>();
+        this.rigidbody = Add<Rigidbody>("PlayBall")!;
+        rigidbody.Bounciness = 1;
         rigidbody.Collide += OnCollide;
     }
 
     private void OnCollide(ICollider obj)
     {
+        Console.WriteLine($"{this} collided with {obj.GameObject}");
     }
 
     public void ReleaseFromBar()
     {
-        rigidbody.Velocity = -Vector3.UnitY;
+        rigidbody.Velocity = -Vector3.UnitY * 0.1f;
     }
 }
